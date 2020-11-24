bf80cc9aec5a1a8b0431bfdfde3e9244d5b56f96
g.eduard <g.eduard@leadsoft.ro>
2020-11-24 10:59:00 +0200

    frontend added

---
+++ frontend/src/views/Classes.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="profile-page">
+    <section class="section-profile-cover section-shaped my-0">
+      <div class="shape shape-style-1 shape-primary shape-skew alpha-4">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </section>
+    <section class="section section-skew">
+      <div class="container">
+        <card shadow class="card-profile mt--300" no-body>
+          <div class="px-4">
+            <div class="row justify-content-center">
+              <div class="col-lg-3 order-lg-2">
+                <div class="card-profile-image">
+                  <a href="#">
+                    <img v-lazy="'img/theme/team-1-800x800.jpg'" class="rounded-circle">
+                  </a>
+                </div>
+              </div>
+              <div class="col-lg-4 order-lg-3 text-lg-right align-self-lg-center">
+                <div class="card-profile-actions py-4 mt-lg-0">
+                </div>
+              </div>
+              <div class="col-lg-4 order-lg-1">
+                <div class="card-profile-stats d-flex justify-content-center">
+                  <div>
+                    <span class="heading">10</span>
+                    <span class="description">Materials</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="text-center mt-5">
+              <h3>Available Classes
+              </h3>
+              <div class="h6 mt-4"><i class="ni business_briefcase-24 mr-2"></i>Distributed Systems Master</div>
+              <div><i class="ni education_hat mr-2"></i>Faculty of Computer Science Iasi Romania</div>
+
+              <table class="table mt-2">
+                <thead>
+                <tr>
+                  <th>Class name</th>
+                  <th>Actions</th>
+                </tr>
+                </thead>
+                <tbody>
+                  <template v-for="myclass in classes">
+                    <tr>
+                      <td>{{myclass.name}}</td>
+                      <td>
+                        <button type="button" rel="tooltip" class="btn btn-primary btn-icon btn-sm btn-simple" data-original-title="" title="">
+                        <i class="ni ni-circle-08 pt-1"></i>
+                      </button></td>
+                    </tr>
+                  </template>
+                </tbody>
+              </table>
+
+            </div>
+            <div class="mt-5 py-5 border-top text-center">
+              <div class="row justify-content-center">
+              </div>
+            </div>
+          </div>
+        </card>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+name: "classes",
+  data() {
+    return {
+      classes: []
+    }
+  },
+  created() {
+    var self = this;
+    this.$route.params.loggedIn = true;
+    axios.get('http://localhost:80/vismart_server/ClassesHandler.php').then(function(response) {
+      self.classes = response.data;
+    })
+  }
+
+}
+</script>
+
+
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,79 @@
+<template>
+    <section class="section section-shaped section-lg my-0">
+        <div class="shape shape-style-1 bg-gradient-default">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <div class="container pt-lg-md">
+            <div class="row justify-content-center">
+                <div class="col-lg-5">
+                    <card type="secondary" shadow
+                          header-classes="bg-white pb-5"
+                          body-classes="px-lg-5 py-lg-5"
+                          class="border-0">
+
+                        <template>
+                            <form role="form">
+                                <base-input alternative
+                                            class="mb-3"
+                                            placeholder="Email"
+                                            addon-left-icon="ni ni-email-83"
+                                v-model="email">
+                                </base-input>
+                                <base-input alternative
+                                            type="password"
+                                            placeholder="Password"
+                                            addon-left-icon="ni ni-lock-circle-open"
+                                v-model="password">
+                                </base-input>
+                                <base-checkbox>
+                                    Remember me
+                                </base-checkbox>
+                                <div class="text-center">
+                                    <base-button type="primary" class="my-4" @click="login">Sign In</base-button>
+                                </div>
+                            </form>
+                        </template>
+                    </card>
+                    <div class="row mt-3">
+                        <div class="col-6">
+                            <a href="#" class="text-light">
+                                <small>Forgot password?</small>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+import axios from 'axios';
+export default {
+  name:"login",
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    login() {
+      var self = this;
+      axios.post('http://localhost:80/vismart_server/LoginHandler.php', {email: this.email, password: this.password}).then(function(response) {
+        if(response.data.success == true) {
+          self.$router.push({ name: 'profile', params: {firstName: response.data.first_name, lastName: response.data.last_name, loggedIn: true}});
+        }
+      })
+    }
+  }
+};
+</script>
+<style>
+</style>
